36f9d4f53acb2a4407d3e67812dd0ddda6ef57c1
Isaac Lucas <isaaclucas.bezerra@gmail.com>
2023-08-20 18:48:39 -0300

    Autenticação simples, cadastro e login sem estilo

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("cadastro/", views.cadastro, name="cadastro"),
+    path('login/', views.login, name='login' ),
+    path('projeto/', views.projeto, name='projeto')
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,9 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.response import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_a
+from django.contrib.auth.decorators import login_required
 
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
 
-# def index(request):
-#     return HttpResponse("Olá, mundo. Você está na página de index do app.")
+        user = User.objects.filter(username=usuario).first()
 
-def home(request):
-    return render(request, 'home.html')
+        if user:
+            return HttpResponse('Usuário ja existe')
+
+        user = User.objects.create_user(username=usuario, password=senha)
+        user.save()
+
+        return HttpResponse('Usuário cadastrado com sucesso!')
+
+def login(request):
+    if request.method =="GET":
+        return render(request, 'login.html')
+    else:
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=usuario, password=senha)
+
+        if user:
+            login_a(request, user)
+            return HttpResponse('Login realizado com sucesso!')
+        else:
+            return HttpResponse('Usuário ou senha inválido.')
+
+@login_required(login_url="/login/")
+def projeto(request):
+    # if request.user.is_authenticated:
+    #     return HttpResponse('Projetos')
+    # else:
+    return HttpResponse('Projeto.')
