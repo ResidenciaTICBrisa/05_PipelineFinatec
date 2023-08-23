1a2e11d6a06615bfb7ac99e92a2411273df3a75b
Isaac Lucas <isaaclucas.bezerra@gmail.com>
2023-08-23 14:38:23 -0300

    Botão de sair funcional sem estilizar

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("cadastro/", views.cadastro, name="cadastro"),
     path('', views.login, name='login' ),
-    path('projeto/', views.projeto, name='projeto')
+    path('projeto/', views.projeto, name='projeto'),
+    path('logout/', views.custom_logout, name='logout'),
 ]
