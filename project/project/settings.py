9a7ea973f7ce7b1472000a4f37b78fff2dbd5263
Hemanoel <hemanoel003@gmail.com>
2023-10-07 01:23:33 +0000

    update: atualizando itens das models apos teste com banco

--- project/project/settings.py
+++ project/project/settings.py
@@ -65,9 +65,17 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'automatec',
+        'USER': 'automauser',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 # DATABASES = {
