654a00ece18fec6b44034f2aa6c552e5d20849fa
Raquel Eucaria <81540491+raqueleucaria@users.noreply.github.com>
2023-12-07 23:41:10 +0000

    excluindo arquivos

--- project/project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-
-urlpatterns = [
-    path('', include('app.urls')),
-    path('admin/', admin.site.urls),
-     path('projetos/',include('backend.urls')),
-]
