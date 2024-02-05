16dc4e235f04874d89b104211d38db5587d8b760
Pedro Barbosa <pedrobarbosaocb@gmail.com>
2024-02-05 20:24:38 +0000

    style: refatorando html base para reset

--- project/app/urls.py
+++ project/app/urls.py
@@ -7,12 +7,14 @@ from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    # path("cadastro/", views.cadastro, name="cadastro"),
+    
     path('login/', views.login, name='login' ),
     path('projeto/', views.projeto, name='projeto'),
     path('logout/', views.custom_logout, name='logout'),
+    # path('base/', views.base2, name='base'),-
+    
     # path('login_teste/', views.login_teste, name='login_teste'),
-    # path('cadastro_teste/', views.cadastro_teste, name='cadastro_teste'),
     # path('projeto_teste/', views.projeto_teste, name='projeto_teste'),
+
     path('user-activity-logs/', user_activity_logs, name='user_activity_logs'),
 ]
